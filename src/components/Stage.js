fbbea90af068ae9b444c34c1c5a1b1d38373cbf8
daneielle-pont <danielleshereepont@outlook.com>
2022-06-03 21:46:34 +1200

    setup stage component

---
+++ src/components/Stage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Cell from './Cell'
+
+// NOTE: proptype checking has not been added, so proptypes cannot be verified as of now.
+
+const Stage = ({ stage }) => (
+  // TODO: mapping through the stage props[erty] to create cells from the stage prop[erty].
+  <div>
+    <Cell />
+  </div>
+)
+
+export default Stage
