22108dcafff516696448237f7603bd1516740f0b
daneielle-pont <danielleshereepont@outlook.com>
2022-06-03 21:43:19 +1200

    add props to display

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
-const Display = () => <div>Display</div>
+// two prop[ertie]s will be passed into the display
+const Display = ({ gameOver, text }) => <div>{text}</div> // return is implicit, so no () needed to encase the div
 
 export default Display
