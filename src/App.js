55278be2402a568d5f3dc4346024d9ae99806bd0
daneielle-pont <danielleshereepont@outlook.com>
2022-06-03 21:38:57 +1200

    setup cell component

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Tetris from '.components/Tetris'
+
+// explicit return
+const App = () => (
+  <div className="App">
+    <Tetris />
+  </div>
+)
+
+export default App
