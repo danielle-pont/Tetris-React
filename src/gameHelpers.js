90d782e4b888b487cf23ccd6f325e466b4de6d37
daneielle-pont <danielleshereepont@outlook.com>
2022-06-04 21:23:49 +1200

    build stage array for width and height of cells

--- src/gameHelpers.js
+++ src/gameHelpers.js
@@ -1,2 +1,12 @@
 export const STAGE_WIDTH = 12
 export const STAGE_HEIGHT = 20
+
+// the stage will be a nested/multidimensional array with rows and columns
+export function createStage() {
+  //creating an array from the stage height. NOTE: this will begin as an empty array.
+  //the inline function will fill the array
+  Array.from(Array(STAGE_HEIGHT), () =>
+    // for each row, we create a new array from the stage width, then FILL it with another array
+    new Array(STAGE_WIDTH).fill([0, 'clear'])
+  )
+}
